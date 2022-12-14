61b025ced3f3ac8b26a58a141c917b48f6fd68e6
Nik_svoboda <svoboda9000@gmail.com>
2022-12-14 16:56:27 +0700

    upd models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,95 +1,65 @@
 const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
-const DatePaj = sequelize.define('DatePaj',{
+//Date
+const Paj = sequelize.define('Paj',{
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     date:{type: DataTypes.STRING},
     time:{type: DataTypes.STRING},
 })
 
-const ScanHostParadise = sequelize.define('ScanHostParadise',{
+//ScanHost
+const Paradise = sequelize.define('Paradise',{
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     ip:{type: DataTypes.STRING},
     mask:{type: DataTypes.STRING},
     dns_hostname:{type: DataTypes.STRING},
 })
 
-const PortsRojus = sequelize.define('PortsRojus',{
+//Ports
+const Rojus = sequelize.define('Rojus',{
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     port:{type: DataTypes.STRING},
     service:{type: DataTypes.STRING},
     protocol:{type: DataTypes.STRING},
 })
 
-const ScanNetworkRaj = sequelize.define('ScanNetworkRaj',{
+//ScanNetwork
+const Raj = sequelize.define('Raj',{
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     body:{type: DataTypes.STRING},
     interface:{type: DataTypes.STRING},
     ip:{type: DataTypes.STRING},
 })
 
-const ScanCennet = sequelize.define('ScanCennet',{
+//Scan
+const Info = sequelize.define('Info', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    ip:{type: DataTypes.STRING, unique: true, allowNull: false},
+    ip:{type: DataTypes.STRING},
+    mask:{type: DataTypes.STRING},
+    interface:{type: DataTypes.STRING},
+    dns_hostname:{type: DataTypes.STRING},
+
 })
 
-const Brand = sequelize.define('brand', {
+//Report
+const Cennet = sequelize.define('Cennet', {
     id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name:{type: DataTypes.STRING, unique: true, allowNull: false},
-})
+    ip:{type: DataTypes.STRING},
+    mask:{type: DataTypes.STRING},
+    interface:{type: DataTypes.STRING},
+    dns_hostname:{type: DataTypes.STRING},
 
-const Rating = sequelize.define('rating', {
-    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name:{type: DataTypes.INTEGER, allowNull: false},
-})
-
-
-const DeviceInfo = sequelize.define('device_info', {
-    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name:{type: DataTypes.STRING, allowNull: false},
-    description:{type: DataTypes.STRING, allowNull: false},
 })
 
 
-const TypeBrand = sequelize.define('type_brand',{
-    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-})
-
-User.hasOne(Basket)
-Basket.belongsTo(User)
-
-User.hasMany(Rating)
-Rating.belongsTo(User)
-
-Basket.hasMany(BasketDevice)
-BasketDevice.belongsTo(Basket)
-
-Type.hasMany(Device)
-Device.belongsTo(Type)
-
-Brand.hasMany(Device)
-Device.belongsTo(Brand)
-
-Device.hasMany(Rating)
-Rating.belongsTo(Device)
-
-Device.hasMany(BasketDevice)
-BasketDevice.belongsTo(Device)
-
-Device.hasMany(DeviceInfo)
-DeviceInfo.belongsTo(Device)
-
-Type.belongsToMany(Brand, {through: TypeBrand})
-Brand.belongsToMany(Type, {through: TypeBrand})
 
 module.exports ={
-    User,
-    Basket,
-    BasketDevice,
-    Device,
-    Type,
-    Brand,
-    Rating,
-    TypeBrand,
-    DeviceInfo
+    Paj,
+    Paradise,
+    Rojus,
+    Raj,
+    Info,
+    Cennet,
+
 }
